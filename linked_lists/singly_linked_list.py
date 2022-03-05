5a2da8506bf703ba36af1ec5ec0663ac29df2263
thepsalmist <xf.xavierfrank@gmail.com>
2022-03-05 05:27:25 +0300

    update

--- linked_lists/singly_linked_list.py
+++ linked_lists/singly_linked_list.py
@@ -3,11 +3,21 @@ Creating a linked list, 2 classes. Create a class for Node object and
 class for the linked list itself (consists of Nodes)
 """
 
+
 class Node:
-    def __init__(self,data) -> None:
+    def __init__(self, data) -> None:
         self.data = data
         self.next = None
+
 
 class LinkedList:
     def __init__(self) -> None:
         self.head = None
+
+    def append(self, data):
+        pass
+
+
+llist = LinkedList
+llist.append("A")
+llist.append("B")
